088d0dcd76b0a6529f00b1776bada9dd6c1140ef
David Venegas <venegasdavidm@gmail.com>
2023-05-23 23:55:57 -0400

    snek

--- main.py
+++ main.py
@@ -13,11 +13,30 @@ BACKGROUND_COLOR = "#000000"  # black
 
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(
+                x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake"
+            )
 
 
 class Food:
-    pass
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+
+        canvas.create_oval(
+            x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food"
+        )
 
 
 def next_turn():
@@ -37,9 +56,31 @@ def game_over():
 
 
 window = Tk()
-
 window.title("Snake Game")
-
 window.resizable(False, False)
+
+score = 0
+direction = "down"
+
+label = Label(window, text="Score: {score}".format(score=score), font=("consolas", 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
