2426c01ca87e75483ddf5d2cce90c2041d8e7537
David Venegas <venegasdavidm@gmail.com>
2023-05-24 00:27:46 -0400

    wurkin

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import random
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
-SPEED = 50
+SPEED = 100
 SPACE_SIZE = 50
 BODY_PARTS = 3
-SNAKE_COLOR = "#00FF00"  # green
-FOOD_COLOR = "#FF0000"  # red
-BACKGROUND_COLOR = "#000000"  # black
+SNAKE_COLOR = "green"
+FOOD_COLOR = "red"
+BACKGROUND_COLOR = "black"
 
 
 class Snake:
@@ -45,43 +45,73 @@ def next_turn(snake, food):
 
     if direction == "up":
         y -= SPACE_SIZE
-
     elif direction == "down":
         y += SPACE_SIZE
-
     elif direction == "left":
         x -= SPACE_SIZE
-
     elif direction == "right":
         x += SPACE_SIZE
 
     snake.coordinates.insert(0, (x, y))
-
     square = canvas.create_rectangle(
         x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR
     )
-
     snake.squares.insert(0, square)
 
-    del snake.coordinates[-1]
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+        global score
+        score += 1
+        label.config(text="Score: {}".format(score))
+        canvas.delete("food")
+        food = Food()
+    else:
+        del snake.coordinates[-1]
+        canvas.delete(snake.squares[-1])
+        del snake.squares[-1]
 
-    canvas.delete(snake.squares[-1])
-
-    del snake.squares[-1]
-
-    window.after(SPEED, next_turn, snake, food)
+    if check_collisions(snake):
+        game_over()
+    else:
+        window.after(SPEED, next_turn, snake, food)
 
 
 def change_direction(new_direction):
-    pass
+    global direction
+
+    if new_direction == "left" and direction != "right":
+        direction = new_direction
+    elif new_direction == "right" and direction != "left":
+        direction = new_direction
+    elif new_direction == "up" and direction != "down":
+        direction = new_direction
+    elif new_direction == "down" and direction != "up":
+        direction = new_direction
 
 
-def check_collisions():
-    pass
+def check_collisions(snake):
+    x, y = snake.coordinates[0]
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
 
 
 def game_over():
-    pass
+    canvas.delete(ALL)
+    canvas.create_text(
+        canvas.winfo_width() / 2,
+        canvas.winfo_height() / 2,
+        font=("consolas", 70),
+        text="GAME OVER",
+        fill="red",
+        tag="game_over",
+    )
 
 
 window = Tk()
