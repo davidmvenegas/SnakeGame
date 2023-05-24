e399c0452a13acd24d3e7f8230c3839396321cf6
David Venegas <venegasdavidm@gmail.com>
2023-05-23 23:37:59 -0400

    vars

--- main.py
+++ main.py
@@ -2,21 +2,44 @@ from tkinter import *
 import random
 
 
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"  # green
+FOOD_COLOR = "#FF0000"  # red
+BACKGROUND_COLOR = "#000000"  # black
+
 
 class Snake:
     pass
 
+
 class Food:
     pass
+
 
 def next_turn():
     pass
 
+
 def change_direction(new_direction):
     pass
+
 
 def check_collisions():
     pass
 
+
 def game_over():
     pass
+
+
+window = Tk()
+
+window.title("Snake Game")
+
+window.resizable(False, False)
+
+window.mainloop()
